74bb6f9cca30c149e27095472f2e15f6491975f9
shanchangshuiyuan <2937958677@qq.com>
2023-12-15 19:07:48 +0800

    second commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>我是App组件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = "我爱你中国";
+
+console.log(str);
+</script>
+
+<style scoped lang="scss"></style>
